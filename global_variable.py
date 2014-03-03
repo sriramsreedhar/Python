88fba3039040059d272633a69f994db577c8e4fe
Sriram Sreedhar <sriramsreedhar@gmail.com>
2014-03-03 08:38:13 -0600

    Global and Local Variable

---
+++ global_variable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+total = 1; # This is global variable.
+# Function definition is here
+def sum( arg1, arg2 ):
+   # Add both the parameters and return them."
+   total = arg1 + arg2; # Here total is local variable.
+   print "Inside the function local total : ", total
+   return total;
+
+# Now you can call sum function
+sum( 10, 20 );
+print "Outside the function global total : ", total
